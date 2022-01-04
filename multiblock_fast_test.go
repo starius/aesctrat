740dbfa000b7f5e43851b06107ea884c10b5e86b
Boris Nagaev <bnagaev@gmail.com>
2022-01-03 22:44:34 -0500

    add test for rev16

--- multiblock_fast_test.go
+++ multiblock_fast_test.go
@@ -102,3 +102,9 @@ func EncryptorBenchmarkCTR(b *testing.B, f CtrEncryptor, blocks int) {
 		f(v.Rounds, &enc[0], &ciphertext[0], &plaintext[0], &iv[0], blockIndex)
 	}
 }
+
+func TestRev16(t *testing.T) {
+	iv := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	rev16Asm(&iv[0])
+	assert.Equal(t, []byte{15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, iv)
+}
